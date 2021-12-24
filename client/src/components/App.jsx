71786dd42b5c5e44c8713193cc22d973789f4dd7
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-12-24 15:27:27 -0800

    refactoring NavBar component using react-router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar.jsx";
+import Services from "./Services.jsx";
 import About from "./About.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <About />
-    </div>
+    <Router>
+      <div className="app" >
+        <Navbar />
+        <About />
+      </div>
+    </Router>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Stack from "@mui/material/Stack";
 import Services from "./Services.jsx";
+import { NavLink } from "react-router-dom";
 import "../../dist/styles.css";
 
 const Navbar = () => {
@@ -11,16 +12,30 @@ const Navbar = () => {
 
 
   return (
-    <div className="navBar">
-      <Stack direction="row" sx={{ justifyContent: "space-evenly", alignItems: "center" }}>
-        <div className="services">Services</div>
-        <div className="title">Princess Nails</div>
-        <div className="contact">Contact</div>
-      </Stack>
-    </div>
+    <nav className="navBar">
+      <div className="nav-container">
+        <NavLink exact to="/services" className="services-link">
+          Services
+        </NavLink>
+        <NavLink exact to="/" className="title-link">
+          Princess Nails
+        </NavLink>
+      </div>
+    </nav>
 
   )
 
 }
 
 export default Navbar;
+
+// return (
+//   <div className="navBar">
+//     <Stack direction="row" sx={{ justifyContent: "space-evenly", alignItems: "center" }}>
+//       <div className="services">Services</div>
+//       <div className="title">Princess Nails</div>
+//       <div className="contact">Contact</div>
+//     </Stack>
+//   </div>
+
+// )
