fd43ca8ce52a4deb310949fb7adce1592ef3ba9e
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-12-24 15:56:51 -0800

    removing unnecessary code and reformatting file structures

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Navbar from "./Navbar.jsx";
-import Services from "./Services.jsx";
 import About from "./About.jsx";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar.jsx";
+import Services from "./Links/Services.jsx";
+import { BrowserRouter as Router, Switch, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      <div className="app" >
-        <Navbar />
-        <About />
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="Services" element={<Services />} />
+        </Routes>
       </div>
     </Router>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import Stack from "@mui/material/Stack";
-import Services from "./Services.jsx";
-import { NavLink } from "react-router-dom";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 import "../../dist/styles.css";
 
-const Navbar = () => {
-  const navigate = useNavigate();
-
-  const homeClick = e => navigate("/");
-
+const NavBar = () => {
 
   return (
     <nav className="navBar">
       <div className="nav-container">
-        <NavLink exact to="/services" className="services-link">
+        <NavLink exact to="/services" className="nav-link">
           Services
         </NavLink>
         <NavLink exact to="/" className="title-link">
           Princess Nails
+        </NavLink>
+        <NavLink exact to="/contact" className="nav-link">
+          Contact
         </NavLink>
       </div>
     </nav>
@@ -27,15 +23,5 @@ const Navbar = () => {
 
 }
 
-export default Navbar;
+export default NavBar;
 
-// return (
-//   <div className="navBar">
-//     <Stack direction="row" sx={{ justifyContent: "space-evenly", alignItems: "center" }}>
-//       <div className="services">Services</div>
-//       <div className="title">Princess Nails</div>
-//       <div className="contact">Contact</div>
-//     </Stack>
-//   </div>
-
-// )
