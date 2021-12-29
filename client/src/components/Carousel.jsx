56976200de12151c010a8d75f4e336aef90dc108
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-12-28 19:06:42 -0800

    created slider images with dummy photos and arrow functionality

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -5,16 +5,27 @@ const Carousel = () => {
   const settings = {
     className: "center",
     centerMode: true,
-    centerPadding: "60px",
+    centerPadding: "10px",
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    swipeToSlide: true
+    slidesToScroll: 1,
+    swipeToSlide: true,
+
   };
 
   return (
-    <div className="slick">
-      <Slider {...settings}>
+    <div className="slick-container">
+      <Slider className="slick-slider" {...settings}>
+        <div>
+          <img src="http://placekitten.com/g/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/g/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/g/400/200" />
+        </div>
         <div>
           <img src="http://placekitten.com/g/400/200" />
         </div>
