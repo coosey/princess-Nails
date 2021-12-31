77af984baf890e0b7d5d0df9c058d9c991870ab7
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-12-30 20:18:30 -0800

    adding social media icons to home page

--- client/src/components/Links/About.jsx
+++ client/src/components/Links/About.jsx
@@ -3,14 +3,10 @@ import React from "react";
 const About = () => {
   return (
       <div className="about-container">
-        <div className="about-Header">About Us...</div>
+        <h4 className="about-Header">About Us...</h4>
         <div className="about-Body">
             Hi! My name is Lisa, as a mother of 3, I opened this family-owned business almost 20 years ago. Many loving customers and friends have still come back to this place to receive the best treatment we can offer! Princess Nails is the only nail salon you need to visit in Murrieta. We specialize in manicures, pedicures, and waxing. Come visit Princess Nails for a relaxing experience!
         </div>
-        {/* <div className="about-Header">About Us...</div>
-        <div className="about-Body">
-          Hi! My name is Lisa, as a mother of 3, I opened this family-owned business almost 20 years ago. Many loving customers and friends have still come back to this place to receive the best treatment we can offer!Princess Nails is the only nail salon you need to visit in Murrieta. We specialize in manicures, pedicures, and waxing. Come visit Princess Nails for a relaxing experience!
-        </div> */}
       </div>
   )
 }
--- client/src/components/Links/Socials.jsx
+++ client/src/components/Links/Socials.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faYelp } from "@fortawesome/free-brands-svg-icons";
 
 const Socials = () => {
   return (
     <div className="social-container">
-      <h3>Follow our Socials!</h3>
+      <h3 className="social-header">Follow our Socials!</h3>
+      <div className="social-body">
+        <a href="https://www.instagram.com/princessnails_murrieta/" className="social-IG">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="https://www.yelp.com/biz/princess-nails-murrieta-2" className="social-Yelp">
+          <FontAwesomeIcon icon={faYelp} size="2x" />
+        </a>
+      </div>
     </div>
   )
 }
