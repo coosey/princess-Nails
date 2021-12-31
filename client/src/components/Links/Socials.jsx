09a088a0a4bd960daf5f58045f59734217911925
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-12-30 16:43:32 -0800

    applied flex display to about us section

--- client/src/components/Links/Socials.jsx
+++ client/src/components/Links/Socials.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Socials = () => {
   return (
     <div className="social-container">
-      <h3>Social Follow</h3>
+      <h3>Follow our Socials!</h3>
     </div>
   )
 }
