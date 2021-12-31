b0b878ed7d70e222cb8ed27bcd1e35e416e190a6
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-12-31 01:03:26 -0800

    styled social colors and modified border styles

--- client/src/components/Links/About.jsx
+++ client/src/components/Links/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
       <div className="about-container">
-        <h4 className="about-Header">About Us...</h4>
+        <h4 className="about-Header">About Us</h4>
         <div className="about-Body">
             Hi! My name is Lisa, as a mother of 3, I opened this family-owned business almost 20 years ago. Many loving customers and friends have still come back to this place to receive the best treatment we can offer! Princess Nails is the only nail salon you need to visit in Murrieta. We specialize in manicures, pedicures, and waxing. Come visit Princess Nails for a relaxing experience!
         </div>
