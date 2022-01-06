d0aa3729ce04c12a7976c99a9f6a8bb1a62e1bd9
matthewnguyen <itsmatthewnguyen@gmail.com>
2022-01-05 17:10:36 -0800

    restyling contact page google map size

--- client/src/components/Links/Contact.jsx
+++ client/src/components/Links/Contact.jsx
@@ -24,19 +24,21 @@ const Contact = () => {
 
   return (
     <div className="contact">
-      <div>
-        <h4 className="contact-location">Location</h4>
-        <iframe className="contact-map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.301835324778!2d-117.18690954901354!3d33.57151158064491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc82703dccff57%3A0x8b0a0aa414b33a4f!2sPrincess%20Nails%20(WE&#39;RE!5e0!3m2!1sen!2sus!4v1641415906986!5m2!1sen!2sus"
-          width="600px"
-          height="450px"
-          style={{ border:"0" }}
-          allowFullScreen=""
-          loading="lazy"
-        >
-        </iframe>
+      <div className="contact-location">
+        <h4 className="contact-location-title">Location</h4>
+        <div className="google-map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.301835324778!2d-117.18690954901354!3d33.57151158064491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dc82703dccff57%3A0x8b0a0aa414b33a4f!2sPrincess%20Nails%20(WE&#39;RE!5e0!3m2!1sen!2sus!4v1641415906986!5m2!1sen!2sus"
+            width="600px"
+            height="450px"
+            style={{ border:"0" }}
+            allowFullScreen=""
+            loading="lazy"
+          >
+          </iframe>
+        </div>
       </div>
-      <div>
+      <div className="information">
         <h4 className="contact-title">Address</h4>
           <div className="contact-info">39872 Los Alamos Rd Ste A3B Murrieta, CA 92562</div>
         <h4 className="contact-title">Phone</h4>
