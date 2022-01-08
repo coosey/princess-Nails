d33ac7d492018f59f6e063a9dee7a4de52d59d44
matthewnguyen <itsmatthewnguyen@gmail.com>
2022-01-08 14:16:48 -0800

    added photo art and fills menu with grid layout

--- client/src/components/Links/Services.jsx
+++ client/src/components/Links/Services.jsx
@@ -6,9 +6,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Typography from "@mui/material/Typography";
 import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 
 const Services = () => {
   const fullSetData = (name, price) => {
+    return { name, price }
+  }
+
+  const fillData = (name, price) => {
     return { name, price }
   }
 
@@ -19,23 +25,54 @@ const Services = () => {
     fullSetData('Powder color', 45)
   ]
 
+  const fills = [
+    fillData("Regular", 30),
+    fillData("Pink", 35),
+    fillData("Pink & white", 45)
+  ]
+
   return (
       <div className="services">
-          <h4 className="services-title">Come get professionally polished.</h4>
-          <div className="services-fullset">
-            <Typography variant="h5" sx={{ textAlign: "center", margin: "5px" }}>Full Set</Typography>
-            <TableContainer component={Paper}>
-              <Table size="medium" aria-label="simple-table">
-                <TableBody>
-                  {fullSets.map(set => (
-                    <TableRow key={set.name}>
-                      <TableCell component="th" scope="row">{set.name}</TableCell>
-                      <TableCell>{set.price}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+          <h4 className="services-title">Come get professionally polished</h4>
+          <div className="fullset-img">
+            <Card>
+              <CardMedia
+                component="img"
+                image="./images/services-nail.jpg"
+              />
+            </Card>
+          </div>
+          <div className="fullset-fill">
+            <div className="services-fullset">
+              <Typography variant="h5" sx={{ textAlign: "center", margin: "15px" }}>Full Set</Typography>
+              <TableContainer component={Paper}>
+                <Table size="medium" aria-label="simple-table">
+                  <TableBody>
+                    {fullSets.map(set => (
+                      <TableRow key={set.name}>
+                        <TableCell component="th" scope="row">{set.name}</TableCell>
+                        <TableCell>{set.price}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+            <div className="services-fill">
+              <Typography variant="h5" sx={{ textAlign: "center", margin: "15px" }}>Fills</Typography>
+                <TableContainer component={Paper}>
+                  <Table size="medium" aria-label="simple-table">
+                    <TableBody>
+                      {fills.map(fill => (
+                        <TableRow key={fill.name}>
+                          <TableCell component="th" scope="row">{fill.name}</TableCell>
+                          <TableCell>{fill.price}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+            </div>
           </div>
       </div>
   )
