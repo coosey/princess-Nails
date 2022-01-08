e72334307cb98815615e7039770a4222d1583a2f
matthewnguyen <itsmatthewnguyen@gmail.com>
2022-01-08 11:28:37 -0800

    working on Services components & added styling to texts

--- client/src/components/Links/Services.jsx
+++ client/src/components/Links/Services.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
 
 const Services = () => {
   return (
-      <div className="services">Services</div>
+      <div className="services">
+          <h4 className="services-title">Come get professionally polished.</h4>
+          <div className="services-container">
+
+          </div>
+      </div>
   )
 }
 
